64eef226dce875ed567902d5c59bca5855ef0b65
Joe Wright <joewright87@gmail.com>
2018-09-03 13:18:58 -0400

    move files around

---
+++ forge.config.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+	packageConfig: {},
+	electronPackagerConfig: {
+		packageManager: 'npm'
+	},
+	makers: [{
+		name: '@electron-forge/maker-squirrel',
+		config: {
+			// https://js.electronforge.io/maker/squirrel/interfaces/makersquirrelconfig
+			authors: 'Your Boi',
+			// TODO: buy a code signing certificate from Digicert
+			// certificateFile: './cert.pfx',
+			// certificatePassword: 'this-is-a-secret',
+			// copyright: '',
+			// description: '',
+			exe: 'my-cool-game',
+			// iconUrl: './haha.ico',
+			// loadingGif: './haha.gif',
+			// name: 'MyCompany.MyProduct.SubProduct.VersionInfo',
+			noMsi: true,
+			// owners: 'Me!',
+			// remoteReleases: 'dang.com',
+			// remoteToken: 'super-secure',
+			setupExe: 'Set-me-up',
+			// setupIcon: './wow.ico',
+			// signWithParams: '',
+			// title: 'Wow!',
+			version: '1.1'
+		}
+	}]
+};
